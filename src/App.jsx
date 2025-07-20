7e2663e8daaddd36c65f95f7033302e63fb38340
William Cao <williamcao.cmu@gmail.com>
2025-07-20 21:44:34 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
